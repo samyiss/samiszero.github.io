81a349b93ddc5042d717a4a0c7a5f0ff8c609098
samy <samylichine.iss@gmail.com>
2024-02-07 23:56:05 -0500

    change ui + fix header bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,15 +5,17 @@ module.exports = {
   ],
   theme: {
     colors: {
-      'midnights': '#364e58',
-      'midnights-light': '#343f51',
+      'midnights': '#d8d5ce',
+      'text-font': '#625548',
+      'TPD': '#9e958b',
+      'TEXT-TPD': '#72665b',
     },
   },
   plugins: [
     require('tailwind-typewriter')({
       wordsets: {
         fruit: {
-          words: ['Well that was supposed to be a serious portfolio but it turns out to be a what i stan in life. P.S: If you keep scrolling you will find the ATWTMVTVFTV.'],
+          words: ['Well that was supposed to be a serious portfolio but it turns out to be a what i stan in life. P.S: If you keep scrolling you will find the ATWTMVTVFTV music video.'],
           delay: 2
         }
       }
